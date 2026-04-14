fdfaa948fdf390e48c71aef22f6d7d373bb88263
Shruti-Chavan-1008 <shrutichavan1975@gmail.com>
2026-04-14 17:03:48 +0530

    card routes is created

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path=require('path');
 const Menu = require('./models/menu');
 const ejsMate=require('ejs-mate');
 
-
+let card=[];
 
 const MONGO_URL="mongodb://127.0.0.1:27017/hotel"
 
@@ -20,7 +20,7 @@ async function main(){
 app.set("view engine",'ejs');
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
- 
+ app.use(express.urlencoded({extended:true}));
 app.engine('ejs',ejsMate);
  
 
@@ -33,6 +33,7 @@ app.get("/Menu", async(req,res)=>{
    res.render("menu.ejs",{ allMenus });
     
 });
+ 
 
 app.get("/category",(req,res)=>{
     res.render("category");
@@ -41,25 +42,36 @@ app.get("/category",(req,res)=>{
 app.get("/Menu/new",(req,res)=>{
     res.render("new");
 });
+ 
+ 
 app.post("/Menu",async(req,res)=>{
    const newMenu=new Menu(req.body);
    await newMenu.save();
    res.redirect("/category");
  
 });
+ 
+ 
 app.get("/Menu/:category",async(req,res)=>{
     const category= req.params.category;
     const allMenus= await Menu.find({ category:category });
     res.render("menu",{ allMenus });
     
 });
-
  
-     
-app.get("/menu/new",(req,res)=>{
-    res.render("New.ejs");
+app.get("/add-to-card/:id", async (req, res) => {
+    let { id } = req.params;
+
+    const item = await Menu.findById(id);
+
+    card.push(item); // store item
+
+    res.redirect("/card");
 });
  
+app.get("/card", (req, res) => {
+    res.render("card.ejs", { card });
+});
 
 app.listen(8080,(req,res)=>{
     console.log("server is listing on 8080")
